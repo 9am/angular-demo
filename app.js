9d30bc83ed7ad1370132885a2a6c636e140033b4
gaobo <gaobo04@baidu.com>
2015-04-15 18:11:09 +0800

    directive angular

--- app.js
+++ app.js
@@ -1,21 +1,27 @@
 (function () {
     var app = angular.module('angularDemo', []);
 
-    app.directive('demoInput', function () {
-        return {
-            restrict: 'AE',
-            templateUrl: 'partials/demo-input.html',
-            scope: {
-                title: '@',
-                inputModel: '='
-            },
-            link: function (scope, element, attrs) {
-                console.log('title:', attrs.title);
-                element.on('click', function () {
-                    element.css('background-color', 'green');
-                });
+    app.controller('sleepCtrl', function ($scope) {
+        $scope.list = [
+            '零只羊',
+            '一只羊',
+            '两只羊',
+            '三只羊',
+            '四只羊',
+            '五只羊',
+            '六只羊',
+            '七只羊',
+            'zzzzzzzzzz'
+        ];
+
+        $scope.isEven = function (item) {
+            for (var i = 0; i < $scope.list.length; i++) {
+                if ($scope.list[i] === item) {
+                    break;
+                }
             }
-        };
+            return (i % 2 === 0);
+        }
     });
 
 })();
