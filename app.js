73e2554138821781990d34a302e212d11055ee35
gaobo <gaobo04@baidu.com>
2015-04-15 16:48:44 +0800

    watch

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 (function () {
     var app = angular.module('angularDemo', []);
 
-    app.controller('InputCtrl', function () {
+    app.controller('InputCtrl', function ($scope) {
+        $scope.inputValue = '试一试';
 
+        $scope.$watch('inputValue', function () {
+            console.log('value: ', $scope.inputValue);
+        });
     });
 
 })();
