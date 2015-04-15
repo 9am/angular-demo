6ce6513aee068e3998867b5d1b5eae86828e69e6
gaobo <gaobo04@baidu.com>
2015-04-15 16:57:46 +0800

    scope father&son

--- app.js
+++ app.js
@@ -1,11 +1,21 @@
 (function () {
     var app = angular.module('angularDemo', []);
 
+    // 父层级
     app.controller('InputCtrl', function ($scope) {
         $scope.inputValue = '试一试';
 
         $scope.$watch('inputValue', function () {
-            console.log('value: ', $scope.inputValue);
+            console.log('father: ', $scope.inputValue);
+        });
+    });
+
+    // 子层级
+    app.controller('SonOfInputCtrl', function ($scope) {
+        $scope.inputValue = '孩子也试一试';
+
+        $scope.$watch('inputValue', function () {
+            console.log('son: ', $scope.inputValue);
         });
     });
 
